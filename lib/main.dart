a5328c4a082cb29f2e6eee423d8ee751d085626a
Jonathan Boisvert <jonathanboisvertx99@icloud.com>
2022-09-04 19:55:32 -0400

    added third route and FAB

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,54 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.greenAccent,
+        appBar: AppBar(
+          title: const Text('Second Route'),
+        ),
+        body: const Center(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              left: 30,
+              bottom: 30,
+              child: FloatingActionButton(
+                heroTag: 'Next',
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ThirdRoute()));
+                },
+                child: const Text('next!'),
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              right: 30,
+              child: FloatingActionButton(
+                heroTag: 'Back',
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Home'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellowAccent,
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('Third Route'),
       ),
       body: Center(
         child: ElevatedButton(
